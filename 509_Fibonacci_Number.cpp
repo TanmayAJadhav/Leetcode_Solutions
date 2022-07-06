3108e641798f4e49377ade18123fa722deb98dea
Tanmay Jadhav <87440930+TanmayAJadhav@users.noreply.github.com>
2022-07-06 11:08:45 +0530

    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Fibonacci Number. Memory Usage: 5.9 MB, less than 41.61% of C++ online submissions

--- 509_Fibonacci_Number.cpp
+++ 509_Fibonacci_Number.cpp
@@ -25,8 +25,14 @@ public:
         }
         else
         {
-            return fib(n-1) + fib(n-2);
+            int prev1 = 0, prev2 = 1, curr = 0;
+            for(int i = 2; i <= n; ++i)
+            {
+                curr = prev1 + prev2;
+                prev1 = prev2;
+                prev2 = curr;
+            }
+            return curr;
         }
-        
     }
 };
