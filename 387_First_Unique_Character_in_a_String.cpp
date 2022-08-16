3b72c9d21f887f3dcd0c3bc0d271523e234c8723
Tanmay Jadhav <87440930+TanmayAJadhav@users.noreply.github.com>
2022-08-16 11:49:48 +0530

    Add files via upload

---
+++ 387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>mp(26,0);
+        for(char ch : s)
+        {
+            mp[ch-'a']++;
+        }
+        
+        for(int i = 0; i < s.size(); ++i)
+        {
+            if(mp[s[i]-'a'] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+        
+        
+    }
+};
