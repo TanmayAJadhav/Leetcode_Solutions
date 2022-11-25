fa2236dfd86f4590e02d13e4637adc99df5f630c
Tanmay Jadhav <87440930+TanmayAJadhav@users.noreply.github.com>
2022-11-25 22:43:37 +0530

    Add files via upload

---
+++ 1678_Goal_Parser_Interpretation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans = "";
+
+        for(int i = 0; i < command.size(); ++i)
+        {
+            if (command[i] == 'G')
+            {
+                ans.push_back('G');
+            }
+            else if (command[i] == '(')
+            {
+                if(command[i+1] == ')')
+                {
+                    ans.push_back('o');
+                    i++;   
+                }
+                else
+                {
+                    ans.push_back('a');
+                    ans.push_back('l');
+                    i += 3;
+                }
+            }
+        }
+        return ans;
+    }
+};
